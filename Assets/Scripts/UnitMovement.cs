b9941aef7573aaae8af4f9e04023bb36fe71d803
Logan M Hood <hoodlma@gmail.com>
2013-09-20 13:46:36 -0400

    Added a test UnitMovement script. Still working on getting it to work on terrain with relief

---
+++ Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A Unit is any soldier, monster, or other agent that moves around the battlefield.
+/// Units do not move autonomously - they must be given a target point by a controller (e.g. the player or an AI).
+/// </summary>
+public class UnitMovement : MonoBehaviour {
+	
+	/// <summary>
+	/// The speed at which this unit will walk to a target point.
+	/// </summary>
+	public float WalkingSpeed = 10f;
+	
+	/// <summary>
+	/// The walking target for this unit.
+	/// </summary>
+	private Vector3 Target;
+	private bool HasTarget;
+	
+	/// <summary>
+	/// The controller that sent the most recent move order.
+	/// </summary>
+	private GameObject MoveController;
+	
+	// Use this for initialization
+	void Start () {
+		HasTarget = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (HasTarget) {
+			MoveTowardsWaypoint(Target);
+			if (HasReachedWaypoint(Target)) {
+				MoveController.SendMessage("UnitFinishedMovement");
+				HasTarget = false;
+				rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePosition;
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Requests that this unit move toward the target point. The unit will get as close to this point as possible.
+	/// </summary>
+	/// <param name='target'>
+	/// The target point, in world coordinates.
+	/// </param>
+	/// <param name='controller'>
+	/// The controller object that is sending the movement request.
+	/// </param>
+	public void GiveWalkingTarget(Vector3 target, GameObject controller) {
+		this.Target = target;
+		this.HasTarget = true;
+		this.MoveController = controller;
+		transform.LookAt(target);
+		rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+	}
+	
+	/// <summary>
+	/// Moves the unit towards the given waypoint in world coordinates.
+	/// </summary>
+	private void MoveTowardsWaypoint(Vector3 waypoint) {
+		Vector3 trajectory = (waypoint - transform.position).normalized;
+		trajectory *= WalkingSpeed * Time.deltaTime;
+		transform.Translate(trajectory, Space.World);
+	}
+	
+	/// <summary>
+	/// Determines whether this unit has reached the specified waypoint in world coordinates.
+	/// </summary>
+	private bool HasReachedWaypoint(Vector3 waypoint) {
+		float distance = Vector3.Distance(waypoint, transform.position);
+		return (distance < 0.5f);
+	}
+}
---
+++ Assets/Scripts/_Testing Scripts/UnitMovementTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Tests the UnitMovement script by sending a test unit to the camera's location if the user presses SPACE.
+/// </summary>
+public class UnitMovementTest : MonoBehaviour {
+	
+	public GameObject TestUnit;
+	
+	private Vector3 Target;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			TestUnitMovement();
+		}
+	}
+	
+	public void UnitFinishedMovement () {
+		float distance = Vector3.Distance(Target, TestUnit.transform.position);
+		string debugString = string.Format("Unit's distance from target point: {0}", distance);
+		print (debugString);
+	}
+	
+	private void TestUnitMovement () {
+		// Our target position will be directly below the camera, so eliminate y component.
+		Target = transform.position;
+		Target.y = 0f;
+			
+		string debugString = string.Format("Target position: {0},{1},{2}", Target.x, Target.y, Target.z);
+		print(debugString);
+		
+		TestUnit.GetComponent<UnitMovement>().GiveWalkingTarget(Target, this.gameObject);
+	}
+}
