0b1b59b11e91b83c31020924b1ccf1ae87245fa4
Logan M Hood <hoodlma@gmail.com>
2013-09-27 00:01:22 -0400

    Added a Controller abstract class for Player and AI controllers to inherit from.

---
+++ Assets/Scripts/_Controllers/Controller.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A controller is a Player or AI that controls Units on the battlefield.
+/// The Unit will notify its Controller when it needs orders, and the Controller
+/// then sends orders to the Unit.
+/// </summary>
+public abstract class Controller : MonoBehaviour {
+	
+	/// <summary>
+	/// Whether this controller is currently giving actions.
+	/// </summary>
+	protected bool Active;
+	
+	/// <summary>
+	/// The unit currently receiving actions from this controller.
+	/// </summary>
+	protected GameObject CurrentUnit;
+	
+	/// <summary>
+	/// Asks the controller to take control of a unit and switch to an Active state.
+	/// </summary>
+	public void TakeControlOf(GameObject unit) {
+		Active = true;
+		CurrentUnit = unit;
+	}
+	
+	/// <summary>
+	/// Requests that this controller release control of its unit and switch to an Inactive state.
+	/// </summary>
+	public void GiveUpControl() {
+		CurrentUnit = null;
+		Active = false;
+	}
+	
+	/// <summary>
+	/// Whether this controller is active.
+	/// </summary>
+	public bool IsActive() {
+		return this.Active;
+	}
+	
+	/// <summary>
+	/// Moves the unit that this controller is currently controlling.
+	/// </summary>
+	protected abstract void MoveUnit();
+}
