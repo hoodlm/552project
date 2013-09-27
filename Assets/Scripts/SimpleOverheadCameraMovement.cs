1b3cff0c62425ae599f1d3bf416779fe900f3ff9
Logan M Hood <hoodlma@gmail.com>
2013-09-27 10:01:21 -0400

    Added zoom functionality to simple camera script

--- Assets/Scripts/SimpleOverheadCameraMovement.cs
+++ Assets/Scripts/SimpleOverheadCameraMovement.cs
@@ -9,6 +9,9 @@ public class SimpleOverheadCameraMovement : MonoBehaviour {
 	public float maxAcceleration = 4f;
 	public float maxMoveSpeed = 4f;
 	public float dragCoefficient = 0.4f;
+	public float zoomSpeed = 5f;
+	public float minHeight = 5f;
+	public float maxHeight = 50f;
 	
 	private Vector3 currentVelocity = Vector3.zero;
 	
@@ -19,13 +22,29 @@ public class SimpleOverheadCameraMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Zooming();
+		SimpleMovement();
+	}
+	
+	private void Zooming() {
+		float zoom = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+		if ((zoom > 0f && transform.position.y >= minHeight) ||
+			(zoom < 0f && transform.position.y <= maxHeight)) {
+			
+			transform.position += transform.forward * zoom * Time.deltaTime;
+		}
+	}
+	
+	private void SimpleMovement() {
 		Vector3 acceleration = AccelerationFromUserInput();
 		Vector3 drag = CalculateDrag();
 		
+		currentVelocity += acceleration * Time.deltaTime;
+		
 		if (acceleration.Equals(Vector3.zero)) {
 			currentVelocity += drag * Time.deltaTime;
-		} else if (currentVelocity.sqrMagnitude <= maxMoveSpeed * maxMoveSpeed) {
-			currentVelocity += acceleration * Time.deltaTime;
+		} else if (currentVelocity.sqrMagnitude >= maxMoveSpeed * maxMoveSpeed) {
+			currentVelocity = currentVelocity.normalized * maxMoveSpeed;
 		}
 		
 		MoveCamera(currentVelocity * Time.deltaTime);
