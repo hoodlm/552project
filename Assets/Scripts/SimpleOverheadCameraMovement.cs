7bb786dedb2dbcba5c55d857e8c60154121a8b4e
Logan M Hood <hoodlma@gmail.com>
2013-09-27 09:34:59 -0400

    Smoothed camera movement

--- Assets/Scripts/SimpleOverheadCameraMovement.cs
+++ Assets/Scripts/SimpleOverheadCameraMovement.cs
@@ -6,7 +6,11 @@ using System.Collections;
 /// </summary>
 public class SimpleOverheadCameraMovement : MonoBehaviour {
 	
-	public float MoveSpeed = 20f;
+	public float maxAcceleration = 4f;
+	public float maxMoveSpeed = 4f;
+	public float dragCoefficient = 0.4f;
+	
+	private Vector3 currentVelocity = Vector3.zero;
 	
 	// Use this for initialization
 	void Start () {
@@ -15,18 +19,31 @@ public class SimpleOverheadCameraMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 trajectory = GetTrajectoryFromUserInput();
-		MoveCamera(trajectory);
+		Vector3 acceleration = AccelerationFromUserInput();
+		Vector3 drag = CalculateDrag();
+		
+		if (acceleration.Equals(Vector3.zero)) {
+			currentVelocity += drag * Time.deltaTime;
+		} else if (currentVelocity.sqrMagnitude <= maxMoveSpeed * maxMoveSpeed) {
+			currentVelocity += acceleration * Time.deltaTime;
+		}
+		
+		MoveCamera(currentVelocity * Time.deltaTime);
 	}
 	
-	private Vector3 GetTrajectoryFromUserInput() {
-		Vector3 trajectory = Vector3.zero;
-		trajectory += Input.GetAxis("Horizontal") * Vector3.right;
-		trajectory += Input.GetAxis("Vertical") * Vector3.forward;
+	private Vector3 AccelerationFromUserInput() {
+		Vector3 localAcceleration = Vector3.zero;
+		localAcceleration += Input.GetAxis("Horizontal") * Vector3.right;
+		localAcceleration += Input.GetAxis("Vertical") * Vector3.forward;
 		
-		trajectory = trajectory.normalized * MoveSpeed * Time.deltaTime;
+		localAcceleration = localAcceleration.normalized * maxAcceleration;
 		
-		return trajectory;
+		return localAcceleration;
+	}
+	
+	private Vector3 CalculateDrag() {
+		Vector3 drag = -currentVelocity;
+		return drag * dragCoefficient;
 	}
 	
 	private void MoveCamera(Vector3 trajectory) {
