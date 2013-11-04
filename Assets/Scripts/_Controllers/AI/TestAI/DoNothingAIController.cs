e4bf891f835aee941c40bdc3b23a244c8a2c8669
Logan M Hood <hoodlma@gmail.com>
2013-11-03 23:54:40 -0500

    Tested new AI model with a simple do-nothing AI

--- Assets/Scripts/_Controllers/AI/TestAI/DoNothingAIController.cs
+++ Assets/Scripts/_Controllers/AI/TestAI/DoNothingAIController.cs
@@ -1,48 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DoNothingAIController : AbstractController {
+public class DoNothingAIController : AbstractAIController {
 	
 	/// <summary>
 	/// How long to delay before ending turn.
 	/// </summary>
 	public float timeToEnd = 1.0f;
 	private float timer;
-	private bool timerStarted;
+	private bool timerRunning;
 	
 	// Use this for initialization
 	void Start () {
 		scheduler = GameObject.FindGameObjectWithTag("BattleManager");
 		ground = GameObject.FindWithTag("Ground");
 		player = GameObject.FindGameObjectWithTag("Player");
-		timerStarted = false;
+		timerRunning = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (inAction && !timerStarted) {
+		if (currentPhase == TurnPhase.WaitingTurn && this.IsInAction()) {
+			currentPhase = TurnPhase.MovingUnit;
 			timer = timeToEnd;
-			timerStarted = true;
-		} else if (inAction && timerStarted) {
+			timerRunning = true;
+		} else if (currentPhase == TurnPhase.MovingUnit
+					&& timerRunning) {
 			timer -= Time.deltaTime;
 			if (timer <= 0f) {
-				currentUnit.SendMessage("HideActiveUnit", SendMessageOptions.RequireReceiver);
-				GiveUpControl();
+				SendMoveOrderToUnit(currentUnit.transform.position);
 			}
-		}		
-	}
-	
-	/// <summary>
-	/// Since the AI never moves the unit, this method body is empty.
-	/// </summary>
-	override protected void SendMoveOrderToUnit(Vector3 target) {
-			
-	}
-	
-	/// <summary>
-	/// Since the AI never moves the unit, this method body is empty.
-	/// </summary>
-	override protected void UnitDoneMoving(UnitMoveResponse response) {
-		
+		} else if (currentPhase == TurnPhase.Attacking) {
+			currentPhase = TurnPhase.WaitingTurn;
+			currentUnit.SendMessage("HideActiveUnit", SendMessageOptions.DontRequireReceiver);
+			GiveUpControl();
+		}	
 	}
 }
