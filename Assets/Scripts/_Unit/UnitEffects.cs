e693e5d66be1ce276c4eeb0c60bf7df2c0108a8b
Logan M Hood <hoodlma@gmail.com>
2013-11-25 23:45:03 -0500

    implemented animation in-game

--- Assets/Scripts/_Unit/UnitEffects.cs
+++ Assets/Scripts/_Unit/UnitEffects.cs
@@ -36,8 +36,9 @@ public class UnitEffects : MonoBehaviour {
 		teamColor = GetComponent<UnitInfo>().controller.teamColor;
 		teamColor.a = 1.0f;
 		attackRadiusColor.a = 1.0f;
+		movementRadiusColor.a = 1.0f;
 		
-		this.gameObject.renderer.material.color = teamColor;
+		//this.gameObject.renderer.material.color = teamColor;
 	}
 	
 	// Update is called once per frame
