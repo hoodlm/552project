9593506356c544f467ccbc0eae918d1ff473684d
Logan M Hood <hoodlma@gmail.com>
2013-12-04 12:03:38 -0500

    Added damage text effect on attacks

---
+++ Assets/Scripts/DamageText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageText : MonoBehaviour {
+	
+	public float lifetime = 1.50f;
+	public float speed = 1.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifetime -= Time.deltaTime;
+		if (lifetime < 0.0f) {
+			GameObject.Destroy(this.gameObject);
+		}
+		transform.Translate(Vector3.up * speed * Time.deltaTime);
+		transform.Translate(Vector3.left * Mathf.Sin(Time.time * 2f) * Time.deltaTime * speed);
+	}
+}
